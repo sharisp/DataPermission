b3c2ab3e0c8cdec9be1f27fda87b4fbce7c19cda
Andrew Wang <wthsir@gmail.com>
2025-09-17 14:40:12 +1000

    feat: doamin layer

---
+++ DataPermission.Domain/Interfaces/IRowPermission.cs
@@ -0,0 +1,15 @@
+﻿using DataPermission.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPermission.Domain.Interfaces
+{
+    public interface IRowPermission
+    {
+        Task AddRowPermission(RowPermission rowPermission);
+        Task DeleteRowPermission(long id);
+    }
+}
