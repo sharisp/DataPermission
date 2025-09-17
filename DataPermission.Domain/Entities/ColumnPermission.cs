d9862b2e8dde66c16e6d4663891c3659a316e2b6
Andrew Wang <wthsir@gmail.com>
2025-09-17 14:38:55 +1000

    feat: data permission entity

---
+++ DataPermission.Domain/Entities/ColumnPermission.cs
@@ -0,0 +1,32 @@
+﻿using Domain.SharedKernel.BaseEntity;
+using Domain.SharedKernel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPermission.Domain.Entities
+{
+    public class ColumnPermission : BaseEntity, IAggregateRoot
+    {
+        public string TableName { get; private set; }
+        public string ColumnName { get; private set; }
+        public string? Description { get; set; }
+        private ColumnPermission() { }
+        public ColumnPermission(string tableName, string columnName)
+        {
+            TableName = tableName;
+            ColumnName = columnName;
+        }
+
+        public void UpdateColumnName(string columnName)
+        {
+            ColumnName = columnName;
+        }
+        public void UpdateTableName(string tableName)
+        {
+            TableName = tableName;
+        }
+    }
+}
---
+++ DataPermission.Domain/Entities/RowPermission.cs
@@ -0,0 +1,48 @@
+﻿using DataPermission.Domain.Enums;
+using Domain.SharedKernel.BaseEntity;
+using Domain.SharedKernel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPermission.Domain.Entities
+{
+    public class RowPermission: BaseEntity, IAggregateRoot
+    {
+      
+        public string TableName { get; private set; }
+        public RowDataScopeEnum DataScopeType { get; private set; }
+        public string? ScopeField { get; private set; }
+        public string? ScopeValue { get; private set; }
+        public string? Description { get; set; }
+
+        private RowPermission() { }
+        public RowPermission(string tableName, RowDataScopeEnum dataScopeType, string? scopeField, string? scopeValue)
+        {
+            TableName = tableName;
+            DataScopeType = dataScopeType;
+            ScopeField = scopeField;
+            ScopeValue = scopeValue;
+        }
+
+        public void UpdateTableName(string tableName)
+        {
+            TableName = tableName;
+        }
+        public void UpdateDataScopeType(RowDataScopeEnum dataScopeType)
+        {
+            DataScopeType = dataScopeType;
+        }
+        public void UpdateScopeField(string? scopeField)
+        {
+            ScopeField = scopeField;
+        }
+        public void UpdateScopeValue(string? scopeValue)
+        {
+            ScopeValue = scopeValue;
+        }
+
+    }
+}
