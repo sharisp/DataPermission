0832e76ca5782151bf3a17fa28c0975f8dbbcb17
Andrew Wang <wthsir@gmail.com>
2025-09-17 14:39:34 +1000

    feat: data permission type

---
+++ DataPermission.Domain/Enums/RowDataScopeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPermission.Domain.Enums
+{
+    public enum RowDataScopeEnum
+    {
+        None = 0,
+        Personal = 1,
+        Department = 2,
+        DepartmentAndSubordinates = 3,        
+        Custom = 4,
+    }
+}
