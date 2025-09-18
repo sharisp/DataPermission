2738af5fe09b625b91afd751cbe967b44be628ac
Andrew Wang <wthsir@gmail.com>
2025-09-18 16:18:29 +1000

    feat: data permission type enum

---
+++ DataPermission.Domain/Enums/DataPermissionTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPermission.Domain.Enums
+{
+    public enum DataPermissionTypeEnum
+    {
+        RowPermission = 1,
+        ColumnPermission = 2
+    }
+}
