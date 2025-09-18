314abf61777b9b8a65315b8a69137cedbbfab1e1
Andrew Wang <wthsir@gmail.com>
2025-09-18 16:19:01 +1000

    feat: domain layer

---
+++ DataPermission.Domain/Services/RowPermissionService.cs
@@ -0,0 +1,26 @@
+﻿using DataPermission.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPermission.Domain.Services
+{
+    public class RowPermissionService
+    {
+        public List<RowPermission> QueryRowPermissionsByUserID(long userId)
+        {
+            List<RowPermission> rowPermissions =new List<RowPermission>();
+
+            return rowPermissions;
+        }
+
+        public List<RowPermission> QueryRowPermissionsByRoleId(long roleId)
+        {
+            List<RowPermission> rowPermissions = new List<RowPermission>();
+
+            return rowPermissions;
+        }
+    }
+}
