123bae8ca0f3e5158b434a1224afdeddee8dd99e
Andrew Wang <wthsir@gmail.com>
2025-09-21 17:20:03 +1000

    feat: query extension

---
+++ DataPermission.Infra/Extensions/QueryableExtensions.cs
@@ -0,0 +1,45 @@
+﻿using DataPermission.Infra.Options;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPermission.Infra.Extensions
+{
+
+    public static class QueryableExtensions
+    {
+        public static async Task<PaginationResponse<T>> ToPaginationResponseAsync<T>(
+     this IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            pageIndex = pageIndex < 1 ? 1 : pageIndex;
+            pageSize = pageSize < 1 ? 10 : pageSize;
+
+            var total = await source.CountAsync();
+
+            var dataList = await source
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PaginationResponse<T>(total,
+               dataList);
+
+            ;
+        }
+
+        public static IQueryable<T> ToPaginationQueryAsync<T>(this IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            if (pageIndex <= 0 || pageSize <= 0)
+            {
+                return source;
+            }
+
+            return source
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize);
+        }
+    }
+}
