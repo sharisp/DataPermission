51085246e224a868ee95b48b25f70799d4ebf8bb
Andrew Wang <wthsir@gmail.com>
2025-09-24 10:39:13 +1000

    feat: api actionfilter

---
+++ DataPermission.Api/ActionFilter/UnitOfWorkActionFilter.cs
@@ -0,0 +1,38 @@
+﻿using Domain.SharedKernel.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DataPermission.Api.ActionFilter;
+
+public class UnitOfWorkActionFilter : IAsyncActionFilter
+{
+    private readonly IUnitOfWork _unitOfWork;
+    // private readonly ILogger<UnitOfWorkActionFilter> _logger;
+
+    public UnitOfWorkActionFilter(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+        //  _logger = logger;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var httpMethod = context.HttpContext.Request.Method;
+
+
+        var resultContext = await next();
+
+        // only commit changes for non-GET requests
+        if (!string.Equals(httpMethod, "GET", StringComparison.OrdinalIgnoreCase) && resultContext.Exception == null)
+        {
+            try
+            {
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                //   _logger.LogError(ex, "Failed to commit UnitOfWork");
+                throw;
+            }
+        }
+    }
+}
