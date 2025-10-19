023714ac81942c97c5c0e79750c7acd0752c6da6
Andrew Wang <wthsir@gmail.com>
2025-10-19 20:35:00 +1100

    test: domain add Mock

---
+++ DomainTests/RowPermissionServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using DataPermission.Domain.Interfaces;
+using DataPermission.Domain.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainTests
+{
+
+    [Collection("Global Test Collection")]
+    public class RowPermissionServiceTest
+    {
+        private readonly Mock<IRowDataPermissionRepository> _rowPermisionRespMock;
+        private readonly RowPermissionService _rowPermissionService;
+
+        public RowPermissionServiceTest()
+        {
+            _rowPermisionRespMock = new Mock<IRowDataPermissionRepository>();
+            _rowPermissionService = new RowPermissionService(_rowPermisionRespMock.Object);
+        }
+        [Fact]
+        public void Test_ExistsConflictAsync_Fail()
+        {
+        
+        }
+    }
+}
